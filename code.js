24b25ca91a207fc4cafe029544e9308fd880cf94
zachgalbs <106509725+a-hiremath@users.noreply.github.com>
2023-04-07 18:45:27 -0700

    Update code.js
    workin like a dawg
    
    🐕

--- code.js
+++ code.js
@@ -1,63 +1,85 @@
-let pOne = document.getElementById("player1");
-let pTwo = document.getElementById("player2");
+// Defining variables
+const pOne = document.getElementById("player1");
+const pTwo = document.getElementById("player2");
+const ball = document.getElementById("ball");
+
 let pOneTop = 10;
 let pTwoTop = 10;
-pTwo.style.top = pTwoTop + "px";
-pTwo.style.right = "10px";
-pOne.style.top = pOneTop + "px";
 let pOneMovingUp = false;
 let pOneMovingDown = false;
 let pTwoMovingUp = false;
 let pTwoMovingDown = false;
-let width = pOne.offsetWidth
-let height = pOne.offsetHeight
 
-document.addEventListener('keydown', function(e) {
-    if (e.key == 's') {
-        pOneMovingDown = true;
-    }
-    if (e.key == 'w') {
-        pOneMovingUp = true;
-    }
-    if (e.key == 'ArrowDown') {
-        pTwoMovingDown = true;
-    }
-    if (e.key == 'ArrowUp') {
-        pTwoMovingUp = true;
-    }
-});
+const paddleWidth = pOne.offsetWidth;
+const paddleHeight = pOne.offsetHeight;
 
-document.addEventListener('keyup', function(e) {
-    if (e.key == 's') {
-        pOneMovingDown = false;
-    }
-    if (e.key == 'w') {
-        pOneMovingUp = false;
-    }
-    if (e.key == 'ArrowDown') {
-        pTwoMovingDown = false;
-    }
-    if (e.key == 'ArrowUp') {
-        pTwoMovingUp = false;
-    }
-});
-
-function movePaddles() {
-    if (pOneMovingUp && parseInt(pOne.style.top) > 10) {
-        pOneTop -= 5;
-    }
-    if (pOneMovingDown && parseInt(pOne.style.top) < window.innerHeight-height-10) {
-        pOneTop += 5;
-    }
-    pOne.style.top = pOneTop + "px";
-
-    if (pTwoMovingUp && parseInt(pTwo.style.top) > 10) {
-        pTwoTop -= 5;
-    }
-    if (pTwoMovingDown && parseInt(pTwo.style.top) < window.innerHeight-height-10) {
-        pTwoTop += 5;
-    }
-    pTwo.style.top = pTwoTop + "px";
+// Initialize paddle positions
+function initPaddlePositions() {
+  pOne.style.top = pOneTop + "px";
+  pTwo.style.top = pTwoTop + "px";
+  pTwo.style.right = "10px";
 }
 
-setInterval(movePaddles, 10);
+initPaddlePositions();
+
+// Keydown event listener
+function onKeyDown(e) {
+  if (e.key === 's') pOneMovingDown = true;
+  if (e.key === 'w') pOneMovingUp = true;
+  if (e.key === 'ArrowDown') pTwoMovingDown = true;
+  if (e.key === 'ArrowUp') pTwoMovingUp = true;
+}
+
+// Keyup event listener
+function onKeyUp(e) {
+  if (e.key === 's') pOneMovingDown = false;
+  if (e.key === 'w') pOneMovingUp = false;
+  if (e.key === 'ArrowDown') pTwoMovingDown = false;
+  if (e.key === 'ArrowUp') pTwoMovingUp = false;
+}
+
+document.addEventListener('keydown', onKeyDown);
+document.addEventListener('keyup', onKeyUp);
+
+// Move paddles based on key events
+function movePaddles() {
+    if (pOneMovingUp && parseInt(pOne.style.top) > 10) {
+      pOneTop -= 5;
+    }
+    if (pOneMovingDown && parseInt(pOne.style.top) < window.innerHeight - paddleHeight - 10) {
+      pOneTop += 5;
+    }
+    pOne.style.top = pOneTop + "px";
+  
+    if (pTwoMovingUp && parseInt(pTwo.style.top) > 10) {
+      pTwoTop -= 5;
+    }
+    if (pTwoMovingDown && parseInt(pTwo.style.top) < window.innerHeight - paddleHeight - 10) {
+      pTwoTop += 5;
+    }
+    pTwo.style.top = pTwoTop + "px";
+  
+    requestAnimationFrame(movePaddles);
+  }
+  
+  // Start moving paddles
+  requestAnimationFrame(movePaddles);
+  
+  // Move the ball
+  function moveBall() {
+    const ballStyle = getComputedStyle(ball);
+    let ballX = parseInt(ballStyle.left);
+    let ballY = parseInt(ballStyle.top);
+    let ballDir = Math.round(Math.random());
+  
+    if (ballDir === 0 && ballX > 10) {
+      ballX -= 5;
+      ball.style.left = ballX + "px";
+    }
+  
+    requestAnimationFrame(moveBall);
+  }
+  
+  // Start moving the ball
+  requestAnimationFrame(moveBall);
+  
