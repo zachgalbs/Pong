32e4c87d336d50a2a10a71cae3c6eac8f20cc489
zachgalbs <zacharythomasgalbraith@gmail.com>
2023-04-08 11:26:43 -0700

    added endgame!

--- code.js
+++ code.js
@@ -13,103 +13,126 @@ let pTwoMovingDown = false;
 const paddleWidth = pOne.offsetWidth;
 const paddleHeight = pOne.offsetHeight;
 
+function main() {
+
 // Initialize paddle positions
 function initPaddlePositions() {
-  pOne.style.top = pOneTop + "px";
-  pTwo.style.top = pTwoTop + "px";
-  pTwo.style.right = "10px";
-}
-
-initPaddlePositions();
-
-// Keydown event listener
-function onKeyDown(e) {
-  if (e.key === 's') pOneMovingDown = true;
-  if (e.key === 'w') pOneMovingUp = true;
-  if (e.key === 'ArrowDown') pTwoMovingDown = true;
-  if (e.key === 'ArrowUp') pTwoMovingUp = true;
-}
-
-// Keyup event listener
-function onKeyUp(e) {
-  if (e.key === 's') pOneMovingDown = false;
-  if (e.key === 'w') pOneMovingUp = false;
-  if (e.key === 'ArrowDown') pTwoMovingDown = false;
-  if (e.key === 'ArrowUp') pTwoMovingUp = false;
-}
-
-document.addEventListener('keydown', onKeyDown);
-document.addEventListener('keyup', onKeyUp);
-
-// Move paddles based on key events
-function movePaddles() {
-    if (pOneMovingUp && parseInt(pOne.style.top) > 10) {
-      pOneTop -= 15;
-    }
-    if (pOneMovingDown && parseInt(pOne.style.top) < window.innerHeight - paddleHeight - 10) {
-      pOneTop += 15;
-    }
     pOne.style.top = pOneTop + "px";
-  
-    if (pTwoMovingUp && parseInt(pTwo.style.top) > 10) {
-      pTwoTop -= 15;
-    }
-    if (pTwoMovingDown && parseInt(pTwo.style.top) < window.innerHeight - paddleHeight - 10) {
-      pTwoTop += 15;
-    }
     pTwo.style.top = pTwoTop + "px";
-  
-    requestAnimationFrame(movePaddles);
+    pTwo.style.right = "10px";
   }
   
-  // Start moving paddles
-  requestAnimationFrame(movePaddles);
+  initPaddlePositions();
   
-// Move the ball initially
-let randomNum = Math.round(Math.random());
-let xDir;
-let yDir;
-if (randomNum == 0) {
-    xDir = "left"
-    yDir = "up"
-}
-if (randomNum == 1) {
-    xDir = "right"
-    yDir = "down"
-}
-// Move the ball and check if the ball has hit the edge
-function moveBall() {
-    const ballStyle = getComputedStyle(ball);
-    const pOneStyle = getComputedStyle(pOne);
-    const pTwoStyle = getComputedStyle(pTwo);
-    let pOneLeft = parseInt(pOneStyle.left);
-    let pOneTop = parseInt(pOneStyle.top);
-    let pTwoLeft = parseInt(pTwoStyle.left);
-    let pTwoTop = parseInt(pTwoStyle.top);
-    let pOneWidth = parseInt(pOneStyle.width);
-    let pOneHeight = parseInt(pOneStyle.height);
-    let ballX = parseInt(ballStyle.left);
-    let ballY = parseInt(ballStyle.top);
-    // move the balls y
-    if (yDir == "down") {ballY += 25;}
-    if (yDir == "up") {ballY -= 25;}
+  // Keydown event listener
+  function onKeyDown(e) {
+    if (e.key === 's') pOneMovingDown = true;
+    if (e.key === 'w') pOneMovingUp = true;
+    if (e.key === 'ArrowDown') pTwoMovingDown = true;
+    if (e.key === 'ArrowUp') pTwoMovingUp = true;
+  }
+  
+  // Keyup event listener
+  function onKeyUp(e) {
+    if (e.key === 's') pOneMovingDown = false;
+    if (e.key === 'w') pOneMovingUp = false;
+    if (e.key === 'ArrowDown') pTwoMovingDown = false;
+    if (e.key === 'ArrowUp') pTwoMovingUp = false;
+  }
+  
+  document.addEventListener('keydown', onKeyDown);
+  document.addEventListener('keyup', onKeyUp);
+  
+  // Move paddles based on key events
+  function movePaddles() {
+      if (pOneMovingUp && parseInt(pOne.style.top) > 10) {
+        pOneTop -= 15;
+      }
+      if (pOneMovingDown && parseInt(pOne.style.top) < window.innerHeight - paddleHeight - 10) {
+        pOneTop += 15;
+      }
+      pOne.style.top = pOneTop + "px";
     
-    // move the balls x
-    if (xDir == "left") {ballX -= 25;}
-    if (xDir == "right") {ballX += 25;}
+      if (pTwoMovingUp && parseInt(pTwo.style.top) > 10) {
+        pTwoTop -= 15;
+      }
+      if (pTwoMovingDown && parseInt(pTwo.style.top) < window.innerHeight - paddleHeight - 10) {
+        pTwoTop += 15;
+      }
+      pTwo.style.top = pTwoTop + "px";
+    
+      requestAnimationFrame(movePaddles);
+    }
+    
+    // Start moving paddles
+    requestAnimationFrame(movePaddles);
+    
+  // Move the ball initially
+  let randomNum = Math.round(Math.random());
+  let xDir;
+  let yDir;
+  if (randomNum == 0) {
+      xDir = "left"
+      yDir = "up"
+  }
+  if (randomNum == 1) {
+      xDir = "right"
+      yDir = "down"
+  }
+  // Move the ball and check if the ball has hit the edge
+  function moveBall() {
+      const ballStyle = getComputedStyle(ball);
+      const pOneStyle = getComputedStyle(pOne);
+      const pTwoStyle = getComputedStyle(pTwo);
+      let pOneLeft = parseInt(pOneStyle.left);
+      let pOneTop = parseInt(pOneStyle.top);
+      let pTwoLeft = parseInt(pTwoStyle.left);
+      let pTwoTop = parseInt(pTwoStyle.top);
+      let pOneWidth = parseInt(pOneStyle.width);
+      let pOneHeight = parseInt(pOneStyle.height);
+      let ballX = parseInt(ballStyle.left);
+      let ballY = parseInt(ballStyle.top);
+      // move the balls y
+      if (yDir == "down") {ballY += 25;}
+      if (yDir == "up") {ballY -= 25;}
+      
+      // move the balls x
+      if (xDir == "left") {ballX -= 25;}
+      if (xDir == "right") {ballX += 25;}
+  
+      // check collision
+      if (ballY < 10) {yDir = "down";}
+      if (ballY > window.innerHeight-10) {yDir = "up";}
 
-    // check collision
-    if (ballY < 10) {yDir = "down";}
-    if (ballY > window.innerHeight-10) {yDir = "up";}
+      // if the ball is at or past the rackets x position,
+      if (ballX + 25 < pOneLeft + pOneWidth) {
+        // if the balls y position is between the top and bottom of the paddle
+        if (ballY <= pOneTop + pOneHeight + 10 && ballY >= pOneTop - 20) {xDir = "right";}
+      }
+      if (ballX >= pTwoLeft + pOneWidth) {
+        if (ballY <= pTwoTop + pOneHeight + 10 && ballY >= pTwoTop - 20) {xDir = "left";}
+      }
+      // checking if ball has hit the edge
+      if (ballX <= 0) {
+        setTimeout(() => {
+          alert("Player two (right side) wins!");
+        }, 100);
+        return;
+      }
+      if (ballX + ball.offsetWidth >= document.documentElement.clientWidth) {
+        setTimeout(() => {
+          alert("Player one (left side) wins!");
+        }, 100);
+        return;
+      }
 
-    if (ballX <= pOneLeft + pOneWidth && (ballY <= pOneTop + pOneHeight + 10 && ballY >= pOneTop - 20)) {xDir = "right"}
-    if (ballX >= pTwoLeft + pOneWidth && (ballY <= pTwoTop + pOneHeight + 10 && ballY >= pTwoTop - 20)) {xDir = "left"}
-    // finalizing the ball position
-    ball.style.top = ballY + "px";
-    ball.style.left = ballX + "px";
-
-    requestAnimationFrame(moveBall);
+      // finalizing the ball position
+      ball.style.top = ballY + "px";
+      ball.style.left = ballX + "px";
+  
+      requestAnimationFrame(moveBall);
+  }
+  // Start moving the ball
+  requestAnimationFrame(moveBall);
 }
-
-// Start moving the ball
-requestAnimationFrame(moveBall);
+main();
